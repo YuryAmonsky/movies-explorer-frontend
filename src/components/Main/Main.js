6953d6e6bc79b6b8a255ab884cf47723a5074fa0
Yury Amonsky <yury.amonsky@yandex.ru>
2022-12-12 22:02:06 +0300

    feat: сверстать секцию AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Promo from './Promo/Promo';
+import AboutProject from './AboutProject/AboutProject';
 
 function Main(){
   return (
-    <Promo className="Promo"/>
+    <main>
+      <Promo />
+      <AboutProject/>
+    </main>    
   );
 }
 
