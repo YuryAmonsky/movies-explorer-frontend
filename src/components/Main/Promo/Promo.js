5005f5ffe566baa4c9748ceedeed84877bb75d3d
Yury Amonsky <yury.amonsky@yandex.ru>
2022-12-18 13:08:52 +0300

    feat: добавить адаптив для компонента Promo

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -5,7 +5,7 @@ import './Promo.css';
 function Promo(){
   return (
     <section className="promo" >           
-      <h1 className="promo__title">Учебный&nbsp;проект&nbsp;студента факультета<br className="new-line"/>Веб-разработки.</h1>
+      <h1 className="promo__title">Учебный проект студента факультета <br className="new-line"/>Веб-разработки.</h1>
       <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <button className="promo__button" type="button">Узнать больше</button>
       <img className="promo__logo" src={promoLogo} alt="силует планеты из слов web"></img>
