3dc9d77850a6f84158e2d9778dd9233e3bf9f784
Yury Amonsky <yury.amonsky@yandex.ru>
2022-12-18 03:13:36 +0300

    feat: сверстать бургер

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import {NavLink} from 'react-router-dom';
 import './Header.css';
 import Logo from '../Logo/Logo';
 
-function Header(props) {
+function Header(props) {  
+  
   return (
     <>      
       <header className={`header ${props.isLanding ? "header_type_landing":""}`}>
@@ -17,11 +18,11 @@ function Header(props) {
             <>
               <NavLink to="/movies" className="nav-bar__link nav-bar__link_theme_light" activeClassName="nav-bar__link_active">Фильмы</NavLink>
               <NavLink to="/saved-movies" className="nav-bar__link nav-bar__link_theme_light" activeClassName="nav-bar__link_active">Сохранённые фильмы</NavLink>
-              <NavLink to="/profile" className="nav-bar__link nav-bar__link_theme_light nav-bar__link_w-icon" activeClassName="nav-bar__link_active">
+              <NavLink to="/profile" className="nav-bar__link nav-bar__link_theme_light nav-bar__link_with-icon" activeClassName="nav-bar__link_active">
                 <span>Аккаунт</span>
                 <div className="nav-bar__link-icon"></div>
               </NavLink>
-              <button className="burger-button" type="button"></button>
+              <button className="burger-button" type="button" onClick={props.onClick}></button>
             </>          
           }
         </nav>        
